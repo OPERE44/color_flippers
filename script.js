9e9800e5beae50103c2ebb4220e64a415698f334
OPERE44 <peteropere3@gmail.com>
2021-08-31 22:39:40 +0300

    Modification

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click",  () => {
+
+  // btn.addEventListener('click', function (e) {
+  //   e.preventDefault();
+
   const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
